cb58df21d0256dc2e1900a09695814cb2556d913
Phong <phongnguyen22588@gmail.com>
2025-04-23 23:59:21 +0700

    connect db and setup env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://rs-phongnq1:Phong859@@nestjs-basic.cka9gg4.mongodb.net/')],
+  //imports: [MongooseModule.forRoot('mongodb+srv://rs-phongnq1:Phong859@@nestjs-basic.cka9gg4.mongodb.net/')],
+  imports: [
+    //MongooseModule.forRoot('mongodb+srv://rs-phongnq1:Phong859@nestjs-basic.kffuogu.mongodb.net/'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      //envFilePath: './.test.env',
+      isGlobal: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');
+  
   app.useStaticAssets(join(__dirname, '..', 'public')); // truy cập js, css, images
   app.setBaseViewsDir(join(__dirname, '..', 'views'));  // views
   app.setViewEngine('ejs');
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
