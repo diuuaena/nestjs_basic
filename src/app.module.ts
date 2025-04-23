3f2561e3d0b7ea8bb465f0c76c96f4fd8119db9c
rs-phongnq1 <phongnq1@runsystem.net>
2025-04-23 17:51:23 +0700

    setup data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://rs-phongnq1:Phong859@@nestjs-basic.cka9gg4.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
