ca1f48c59a79c4f5b31d4cc347eabdf2897b4e96
Phong <phongnguyen22588@gmail.com>
2025-06-05 08:18:34 +0700

    module company init

---
+++ src/companies/schemas/company.schema.ts
@@ -0,0 +1,49 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type CompanyDocument = HydratedDocument<Company>;
+
+@Schema({timestamps: true})
+export class Company {
+  @Prop()
+  name: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop({type: Object})
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  }
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop({type: Object})
+  updatedABy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  }
+
+  @Prop()
+  deletedAt: Date;
+
+  @Prop({type: Object})
+  deletedABy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  }
+
+  @Prop()
+  isDeleted: boolean;
+
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,8 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty()
+    @IsEmail({}, { message: 'Email không đúng định dạng'})
+    @IsNotEmpty({ message: 'Email không được để trống'})
     email: string;
 
     @IsNotEmpty()
